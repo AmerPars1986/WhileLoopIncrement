a1c0790c1ad7e148aeae3ec54820f1a21ef42a32
AmerPars1986 <107315391+AmerPars1986@users.noreply.github.com>
2022-06-16 18:01:05 +0100

    Main

---
+++ BreakContinue/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BreakContinue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int i = -10;
+
+            while (i <= 10)
+            {
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine("{0} was skipped", i);
+                    i++;
+                    continue;
+                }
+                else
+                {
+                    Console.WriteLine(i++);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
